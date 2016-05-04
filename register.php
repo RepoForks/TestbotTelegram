98db53c57fde2b13cbff32ad8f43c4ebd885a9a4
Acciaro Gennaro Daniele <acciarogennaro@gmail.com>
2016-05-04 22:17:23 +0200

    d

--- register.php
+++ register.php
@@ -2,7 +2,7 @@
 echo "ciao";
 // PARAMETRI DA MODIFICARE
 $WEBHOOK_URL = 'https://testamiquesto.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$BOT_TOKEN = '225467836:AAGbTlo5GMhdt0tRxqYPWucQZ73cs1COTHw';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
