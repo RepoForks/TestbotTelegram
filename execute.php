2a22eabc9f37c5cac1d4d7ec2dbf7b69924f68b6
Acciaro Gennaro Daniele <acciarogennaro@gmail.com>
2016-05-05 23:23:31 +0200

    cmnac

--- execute.php
+++ execute.php
@@ -39,11 +39,7 @@ if(substr($text, 0,1)=="/"){
         $response.="\nA Creepypasta is a short story that was designed to terrorize the reader
 \nThis application is a database of scary stories in 3 languages (English,Italian and Español).
 \nIt includes over 3500 stories, which will not let you sleep at night\n\n\nDownload here:\nhttps://play.google.com/store/apps/details?id=com.acciarogennaro.creepypasta";
-
-    /*    $response.="🚘AUTOSCUOLA KRISTALL🚘";
-        $response.="\nWith this app you can view a complete video course to get a driving license.
-\nThis includes quizzes and all the tools dedicated for each young driver\n\n\nDownload here:\nhttps://play.google.com/store/apps/details?id=autoscuolakristall.android";
-*/
+        
     break;
 
     default:
@@ -53,8 +49,6 @@ if(substr($text, 0,1)=="/"){
   header("Content-Type: application/json");
   $parameters = array('chat_id' => $chatId, "text" => $response);
   $parameters["method"] = "sendMessage";
-  //$parameters = array('chat_id' => $chatId, "sticker" => $arrayName = array('file_id' =>"BQADBAADgAIAAmbFbQYjv4eHMUpKPQI"));
-  //$parameters["method"] = "sendMessage";
   echo json_encode($parameters);
 
 }else{
